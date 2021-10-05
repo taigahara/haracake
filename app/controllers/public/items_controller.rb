8e95c83530fa834f034fb71690ca5c48152ec8a8
harataiga <harataiga1@MacBook-Air-2.local>
2021-10-05 23:09:54 +0900

    ジャンル検索

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
+    @genres = Genre.all
+
+    if params[:genre_id]
+      @genre = @genres.find(params[:genre_id])
+      @items = @genre.items.where(is_sale: true)
+    else
+      @items = Item.where(is_sale: true)
+    end
   end
   
   def show
-    # binding.pry
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
     session[:item_id] = @item.id
